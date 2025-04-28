54eb8854bb51915a8bcbb74ee10ea6b065aa5374
ali6a7 <168389504+ali6a7@users.noreply.github.com>
2025-04-28 05:47:22 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { support_role_id, ticket_category_id } = require('./config.json');
 
 client.on('message', async message => {
-    if (message.content.toLowerCase() === '!ticket') {
-        const channel = await message.guild.channels.create(`ticket-${message.author.username}`, {
+    if (message.content.toLowerCase() === '!تكت') {
+        const channel = await message.guild.channels.create(`تكت-${message.author.username}`, {
             type: 'text',
             parent: ticket_category_id,
             permissionOverwrites: [
@@ -24,17 +23,17 @@ client.on('message', async message => {
             ],
         });
 
-        channel.send(`<@&${support_role_id}> New ticket opened by ${message.author.tag}`);
-        channel.send('Welcome to your ticket. A support team member will be with you shortly.');
+        channel.send(`<@&${support_role_id}> تم فتح تكت جديد بواسطة ${message.author.tag}`);
+        channel.send('مرحبًا بك في تكتك، سوف يتواصل معك أحد أعضاء الدعم قريبًا.');
     }
 });
 
 client.on('message', message => {
-    if (message.content.toLowerCase() === '!close') {
+    if (message.content.toLowerCase() === '!إغلاق') {
         message.channel.updateOverwrite(message.guild.id, {
             SEND_MESSAGES: false,
         });
-        message.channel.send('Ticket closed.');
+        message.channel.send('تم إغلاق التكت.');
     }
 });
 
